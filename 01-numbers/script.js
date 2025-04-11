f7fa4010c3b4099b96dbefc3baadfa03bdee1087
federicapaglia96 <federicapaglia96@gmail.com>
2025-04-11 14:19:55 +0200

    number

--- 01-numbers/script.js
+++ 01-numbers/script.js
@@ -6,7 +6,7 @@ const numThree = 32; // NON MODIFICARE QUESTA RIGA
 // gli operatori numerici di Javascript
 
 // 1. Imposta questa variabile come somma di numOne e numTwo
-const numOnePlusNumTwo = NaN;
+const numOnePlusNumTwo = numOne + numTwo; // 8 + 16 = 24
 console.log(numOnePlusNumTwo); // Output: 24
 
 // 2. Imposta questa variabile come prodotto di numThree e numTwo
@@ -14,17 +14,17 @@ const numThreeTimesNumTwo = NaN;
 console.log(numThreeTimesNumTwo); // Output: 512
 
 // 3. Imposta questa variabile come divisione di numThree per numOne
-const numThreeDividedByNumOne = NaN;
+const numThreeDividedByNumOne = numThree / numOne; 
 console.log(numThreeDividedByNumOne); // Output: 4
 
 // 4. Imposta questa variabile come differenza tra numThree e numOne
-const numThreeMinusNumOne = NaN;
+const numThreeMinusNumOne = numThree - numOne; // 32 - 8 = 24
 console.log(numThreeMinusNumOne); // Output: 4
 
 // 5. Imposta questa variabile come somma di numOne, numTwo e numThree
-const sum = NaN;
+const sum = numOne + numTwo + numThree; // 8 + 16 + 32 = 56
 console.log(sum); // Output: 56
 
 // 6. Imposta questa variabile come somma di numOne, numTwo, numThree diviso numOne
-const numBytes = NaN;
+const numBytes = (numOne + numTwo + numThree) / numOne; // (8 + 16 + 32) / 8 = 7
 console.log(numBytes); // Output: 7

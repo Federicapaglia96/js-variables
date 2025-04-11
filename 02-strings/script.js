37d62ff2d8daad2e530b9d1de625914a1b3fc626
federicapaglia96 <federicapaglia96@gmail.com>
2025-04-11 17:33:52 +0200

    string-numbers

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -28,11 +28,11 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = firstName + ' ' + lastName; // Concatenazione delle stringhe
+const fullName = firstName + ' ' + lastName;
 
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
